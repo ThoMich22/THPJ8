f35f0552d5a56612f71dca357b230b9fc4471d52
ThoMich22 <thomas_8_69@hotmail.com>
2021-04-14 17:35:55 +0200

    exo hello ok

--- Lib/00_hello.rb
+++ Lib/00_hello.rb
@@ -1,3 +1,31 @@
-def say_hello
-    puts "Bonjour!"
+
+def ask_first_name
+    puts "Quel est ton prénom ?"
+    print "> "
+    first_name = gets.chomp
+    return first_name
   end
+
+  def ask_last_name
+    puts "Quel est ton nom ?"
+    print "> "
+    last_name = gets.chomp
+    return last_name
+  end
+
+  def say_hello
+    print "Bonjour"
+  end
+
+  def greet(hello, first_name, last_name)
+    puts "#{say_hello}, #{first_name} #{last_name} !"
+  end
+
+  def perform
+    say_hello = "Bonjour"
+    first_name = ask_first_name
+    last_name = ask_last_name
+    greet(say_hello, first_name, last_name)
+  end
+  
+  perform
